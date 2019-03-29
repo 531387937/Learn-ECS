ce555c8a19b827dc94b258e7e104622b610af91d
Qizi <531387937@qq.com>
2019-03-30 01:20:37 +0800

    我好了，我懂了（？？？

---
+++ ECS_Test/ECS_Test/Assets/Scripts/SpawnNumberBlocks.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Rendering;
+public class SpawnNumberBlocks : MonoBehaviour
+{
+    public static Texture2D Heightmap;
+
+    public static EntityArchetype BlockArchetype;
+
+    public int ChunkBase = 1;
+
+    public Mesh blockMesh;
+
+    public Material Mat0;
+    public Material Mat1;
+    public Material Mat2;
+    public Material Mat3;
+    public Material Mat4;
+    public Material Mat5;
+    public Material Mat6;
+    public Material Mat7;
+
+    Material maTemp;
+    public EntityManager manager;
+    public Entity entities;
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void Initialize()
+    {
+        EntityManager manager = World.Active.GetOrCreateManager<EntityManager>();
+
+        BlockArchetype = manager.CreateArchetype(typeof(Position));
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = World.Active.GetOrCreateManager<EntityManager>();
+
+        ChunckGenerator(ChunkBase);
+    }
+
+    void ChunckGenerator(int amount)
+    {
+        int heightlevel;
+        bool airChecker;
+
+        for(int yBlock=0;yBlock<15;yBlock++)
+        {
+            for(int xBlock=0;xBlock<10*amount;xBlock++)
+            {
+                for(int zBlock=0;zBlock<10*amount;zBlock++)
+                {
+                    heightlevel = (int)(Heightmap.GetPixel(xBlock, zBlock).r * 100) - yBlock;
+                    airChecker = false;
+                    Vector3 posTemp = new Vector3(xBlock, yBlock, zBlock);
+
+                    switch(heightlevel)
+                    {
+                        case 0:
+                            maTemp = Mat0;
+                            break;
+                        case 1:
+                            maTemp = Mat1;
+                            break;
+                        case 2:
+                            maTemp = Mat2;
+                            break;
+                        case 3:
+                            maTemp = Mat3;
+                            break;
+                        case 4:
+                            maTemp = Mat4;
+                            break;
+                        case 5:
+                            maTemp = Mat5;
+                            break;
+                        case 6:
+                            maTemp = Mat6;
+                            break;
+                       default:
+                            maTemp = Mat7;
+                            airChecker = true;
+                            break;
+                    }
+
+                    if(!airChecker)
+                    {
+                        entities = manager.CreateEntity(BlockArchetype);
+                        manager.SetComponentData(entities, new Position { Value = new int3(xBlock, yBlock, zBlock) });
+                        manager.AddComponentData(entities, new BlockTag { });
+
+                        manager.AddSharedComponentData(entities, new MeshInstanceRenderer
+                        {
+                            mesh = blockMesh, material = maTemp
+                        });
+                    }
+                }
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ECS_Test/ECS_Test/Assets/Scripts/SpwanOneBlock.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Rendering;
+public class SpwanOneBlock : MonoBehaviour
+{
+    private float a = 0;
+
+    public static EntityArchetype BlockArchetype;
+
+    public Mesh BlockMesh;
+    public Material BlockMaterial;
+    public EntityManager manager;
+    public Entity entities;
+    public GameObject prefeb_ref;
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void Initialize()
+    {
+        EntityManager manager = World.Active.GetOrCreateManager<EntityManager>();
+
+        BlockArchetype = manager.CreateArchetype(typeof(Position));
+        
+    }
+
+    private void Start()
+    {
+        //manager = World.Active.GetOrCreateManager<EntityManager>();
+        //Entity[] entities = new Entity[1];
+        //for (int i = 0; i < entities.Length; i++)
+        //{
+        //    entities[i] = manager.CreateEntity(BlockArchetype);
+        //    manager.SetComponentData(entities[i], new Position { Value = new int3(i, 0, 0) });
+        //    manager.AddComponentData(entities[i], new BlockTag { });
+
+        //    manager.AddSharedComponentData(entities[i], new MeshInstanceRenderer
+        //    {
+        //        mesh = BlockMesh,
+        //        material = BlockMaterial
+        //    });
+        //}
+        manager = World.Active.GetOrCreateManager<EntityManager>();
+
+        entities = manager.CreateEntity(BlockArchetype);
+        manager.SetComponentData(entities, new Position { Value = new int3(2, 0, 0) });
+        manager.AddComponentData(entities, new BlockTag { });
+
+        manager.AddSharedComponentData(entities, new MeshInstanceRenderer
+        {
+            mesh = BlockMesh,
+            material = BlockMaterial
+        });
+        if (prefeb_ref)
+        {
+            NativeArray<Entity> entityArray = new NativeArray<Entity>(1, Allocator.Temp);
+            manager.Instantiate(prefeb_ref, entityArray);
+
+            //manager.SetComponentData(entityArray[0], new Position { Value = new int3(4, 0, 0) });
+            entityArray.Dispose();
+        }
+    }
+    private void Update()
+    {
+    }
+}
